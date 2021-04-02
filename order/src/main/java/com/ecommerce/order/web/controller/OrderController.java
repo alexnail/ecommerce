19f679c27c960b19a553d882539fe1f5e36a7f96
Aleksandrs Fridmans <alexnail@gmail.com>
2021-04-02 13:06:57 +0100

    Code cleanup

--- order/src/main/java/com/ecommerce/order/web/controller/OrderController.java
+++ order/src/main/java/com/ecommerce/order/web/controller/OrderController.java
@@ -1,11 +1,8 @@
 package com.ecommerce.order.web.controller;
 
-
 import com.ecommerce.order.dao.OrderDao;
 import com.ecommerce.order.model.Order;
-import com.ecommerce.order.web.exceptions.ImpossibleAjouterOrderException;
 import com.ecommerce.order.web.exceptions.OrderNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,37 +12,27 @@ import java.util.Optional;
 @RestController
 public class OrderController {
 
-    @Autowired
-    OrderDao OrdersDao;
+    private final OrderDao orderDao;
 
-    @PostMapping (value = "/Orders")
-    public ResponseEntity<Order> addOrder(@RequestBody Order Order){
-
-        Order nouvelleOrder = OrdersDao.save(Order);
-
-        if(nouvelleOrder == null) throw new ImpossibleAjouterOrderException("Impossible d'ajouter cette Order");
-
-        return new ResponseEntity<Order>(Order, HttpStatus.CREATED);
+    public OrderController(OrderDao orderDao) {
+        this.orderDao = orderDao;
     }
 
-    @GetMapping(value = "/Orders/{id}")
+    @PostMapping (value = "/orders")
+    public ResponseEntity<Order> addOrder(@RequestBody Order order){
+        Order newOrder = orderDao.save(order);
+        return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/orders/{id}")
     public Optional<Order> findById(@PathVariable int id){
-
-        Optional<Order> Order = OrdersDao.findById(id);
-
-        if(!Order.isPresent()) throw new OrderNotFoundException("Cette Order n'existe pas");
-
-        return Order;
+        Optional<Order> order = orderDao.findById(id);
+        if(!order.isPresent()) throw new OrderNotFoundException("This order doesn't exist");
+        return order;
     }
 
-    /*
-    * Permet de mettre à jour une Order existante.
-    * save() mettra à jours uniquement les champs renseignés dans l'objet Order reçu. Ainsi dans ce cas, comme le champs date dans "Order" n'est
-    * pas renseigné, la date précédemment enregistrée restera en place
-    **/
-    @PutMapping(value = "/Orders")
-    public void updateOrder(@RequestBody Order Order) {
-
-        OrdersDao.save(Order);
+    @PutMapping(value = "/orders")
+    public void updateOrder(@RequestBody Order order) {
+        orderDao.save(order);
     }
 }
